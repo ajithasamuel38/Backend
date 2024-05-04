bbe999ee76719eb1449dd48d3485baf4c17ab8dc
ajithasamuel38 <ajithasamuel38@gmail.com>
2024-05-04 11:38:52 +0530

    update app
    uodate

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const cors = require('cors');
 const app = express();
 //app.use(helmet());
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
-app.use(morgan('combined', {stream: accessLogStream}));
+//const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+//app.use(morgan('combined', {stream: accessLogStream}));
 app.use(bodyParser.json());
 
 app.use(cors());
