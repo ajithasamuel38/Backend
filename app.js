01154a9391685c822dc88c5f285a09c4aa69d994
ajithasamuel38 <ajithasamuel38@gmail.com>
2024-05-12 23:47:38 +0530

    appcid

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(forgotpassroutes);
 app.use(errorController.get404);
 
 app.get('*', (req, res) => {
-    console.log('Url', req.url);
+    console.log('url', req.url);
     res.sendFile(path.join(__dirname, 'public', `${req.url}`));
 });
 
